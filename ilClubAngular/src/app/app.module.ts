5bf0791a25fe2e309ec2463b7524365ea6a9e952
Luca Leone <lucaleone@outlook.com>
2017-11-27 17:04:27 +0100

    merge luca and alessia app module

--- ilClubAngular/src/app/app.module.ts
+++ ilClubAngular/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { routing } from './app.routes';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {routing} from './app.routes';
 
-import { AppComponent } from './app.component';
-import { PageHomeComponent } from './Pages/page-home/page-home.component';
-import { PageNeweventComponent } from './Pages/page-newevent/page-newevent.component';
-import { PageNotfoundcomponentComponent } from './Pages/page-notfoundcomponent/page-notfoundcomponent.component';
-import { PageProfileComponent } from './Pages/page-profile/page-profile.component';
-import { HeaderComponent } from './header/header.component';
-import { SideMenuComponent } from './sidemenu/sidemenu.component';
-import { CalendarComponent } from './calendar/calendar.component';
-<<<<<<< HEAD
-import { PageLoginComponent } from './Pages/page-login/page-login.component';
-=======
->>>>>>> 362946196cbf8f96ac3534cc2ed59cb76482fcb6
+import {AppComponent} from './app.component';
+import {PageHomeComponent} from './Pages/page-home/page-home.component';
+import {PageNeweventComponent} from './Pages/page-newevent/page-newevent.component';
+import {PageNotfoundcomponentComponent} from './Pages/page-notfoundcomponent/page-notfoundcomponent.component';
+import {PageProfileComponent} from './Pages/page-profile/page-profile.component';
+import {HeaderComponent} from './header/header.component';
+import {SideMenuComponent} from './sidemenu/sidemenu.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {PageLoginComponent} from './Pages/page-login/page-login.component';
+import {FirebaseService} from './firebase.service';
+import {NewEventGuard} from './Pages/page-newevent/newevent.guard';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,11 @@ import { PageLoginComponent } from './Pages/page-login/page-login.component';
     HttpModule,
     routing
   ],
+  providers: [
+    FirebaseService,
+    NewEventGuard
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
