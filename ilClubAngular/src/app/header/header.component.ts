508b1288e5bf5cdda6f941980f649c52bef0faa6
Luca Leone <lucaleone@outlook.com>
2017-11-28 14:10:47 +0100

    fix pulsante home

--- ilClubAngular/src/app/header/header.component.ts
+++ ilClubAngular/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  menuOpen = false;
 
-  constructor() { }
+  constructor() {
+  }
+
+  menuClicked() {
+    this.menuOpen = !this.menuOpen;
+  }
 
   ngOnInit() {
   }
